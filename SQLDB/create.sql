18eef03cab85ac79dc109542909ef9d7d7c9d91d
llegro <llegro42895@hotmail.com>
2017-04-07 17:34:37 -0400

    SQLite DB

---
+++ SQLDB/create.sql
@@ -0,0 +1,8 @@
+INSERT INTO profiles (user_id,user_name,first_name,last_name,pref_time,known_lang,learn_lang)
+VALUES ('0000', 'jkong', 'Jacky', 'Kong', '03:00','MANDARIN','SPANISH');
+
+INSERT INTO profiles (user_id,user_name,first_name,last_name,pref_time,known_lang,learn_lang)
+VALUES ('0001', 'llegro', 'Luis', 'Legro', '03:00','SPANISH','MANDARIN');
+
+INSERT INTO profiles (user_id,user_name,first_name,last_name,pref_time,known_lang,learn_lang)
+VALUES ('0002', 'hstolz', 'Henry', 'Stolz', '04:00','ICELANDIC','JAPANESE');
