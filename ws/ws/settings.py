6ab8bdec3c69ae370cec428a311dc666e0e28136
Henry Stolz <situhan@nat-oitwireless-inside-vapornet100-c-11510.Princeton.EDU>
2017-04-13 00:15:31 -0400

    Fixed sqlite -> MySQL for deployed version

--- ws/ws/settings.py
+++ ws/ws/settings.py
@@ -75,19 +75,36 @@ WSGI_APPLICATION = 'ws.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'USER': os.getenv('wu_user'),
-        # 'PASSWORD': os.getenv('wu_password'),
-        'HOST': '127.0.0.1',
-        'USER': 'wu_user',
-        'PASSWORD': 'wu_password',
-        # 'HOST': '/cloudsql/wordup-163921:us-central1:wordup-database',
-        'PORT': '3306',
+if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/wordup-163921:us-central1:wordup-database',
+            'NAME': 'wu_db',
+            'USER': 'wu_user',
+            'PASSWORD': 'wu_password',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'HOST': '127.0.0.1',
+            'USER': 'wu_user',
+            'PASSWORD': 'wu_password',
+            'PORT': '3306',
+            # 'USER': os.getenv('wu_user'),
+            # 'PASSWORD': os.getenv('wu_password'),
+            # 'HOST': '/cloudsql/wordup-163921:us-central1:wordup-database',
+        }
+    }
 
 
 # Password validation
