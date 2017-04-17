8460481735be131a91d589daa44d743f13d731c7
Henry Stolz <situhan@nat-oitwireless-inside-vapornet100-c-11510.Princeton.EDU>
2017-04-16 21:57:16 -0400

    Half-reverted changes
    Using old implementation of Profiles endpoints, new (only) method of
    implementing matches

--- ws/demo/urls.py
+++ ws/demo/urls.py
@@ -3,10 +3,20 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from demo import views
 
 urlpatterns = [
-    url(r'^profiles/$', views.ProfileList.as_view()),
-    url(r'^profiles/(?P<pk>[0-9]+)/$', views.ProfileDetail.as_view()),
+    # url(r'^profiles/$', views.ProfileList.as_view()),
+    # url(r'^profiles/(?P<pk>[0-9]+)/$', views.ProfileDetail.as_view()),
+    url(r'^profiles/$', views.profile_list),
+	url(r'^profiles/(?P<pk>[0-9]+)/$', views.profile_detail),
     url(r'^matches/$', views.MatchList.as_view()),
     url(r'^matches/(?P<pk>[0-9]+)/$', views.MatchDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+# from django.conf.urls import url
+# from demo import views
+
+# urlpatterns = [
+#     url(r'^profiles/$', views.profile_list),
+#     url(r'^profiles/(?P<pk>[0-9]+)/$', views.profile_detail),
+# ]
--- ws/demo/views.py
+++ ws/demo/views.py
@@ -3,20 +3,27 @@ from demo.serializers import ProfileSerializer, MatchSerializer
 from rest_framework import generics, status
 from rest_framework.response import Response
 
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from demo.models import Profile
+from demo.serializers import ProfileSerializer
 
-class ProfileList(generics.ListCreateAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
+# class ProfileList(generics.ListCreateAPIView):
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
 
-class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
+# class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
 
 class MatchList(generics.ListCreateAPIView):
 	queryset = Match.objects.all()
 	serializer_class = MatchSerializer
 
 	def create(self, request):
+		print str(request.data)
 		user_id = request.data['user_id']
 		learn_lang = request.data['learn_lang']
 		initiator = Profile.objects.get(user_id=user_id)
@@ -34,3 +41,41 @@ class MatchList(generics.ListCreateAPIView):
 class MatchDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Match.objects.all()
 	serializer_class = MatchSerializer
+
+@csrf_exempt
+def profile_list(request):
+	if request.method == 'GET':
+		profile = Profile.objects.all()
+		serializer = ProfileSerializer(profile, many=True)
+		return JsonResponse(serializer.data, safe=False)
+
+	elif request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = ProfileSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=201)
+		return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def profile_detail(request, pk):
+	try:
+		profile = Profile.objects.get(pk=pk)
+	except Profile.DoesNotExist:
+		return HttpResponse(status=404)
+
+	if request.method == 'GET':
+		serializer = ProfileSerializer(profile)
+		return JsonResponse(serializer.data)
+
+	elif request.method == 'PUT':
+		data = JSONParser().parse(request)
+		serializer = ProfileSerializer(profile, data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=400)
+
+	elif request.method == 'DELETE':
+		profile.delete()
+		return HttpResponse(status=204)
